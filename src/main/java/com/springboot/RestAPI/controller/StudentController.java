01bbe9ec854548f60891290b17b5b1217c3f7baa
ravi chandola <ravichandola94@gmail.com>
2023-09-05 14:55:20 +0530

    Return List in json format /students

--- src/main/java/com/springboot/RestAPI/controller/StudentController.java
+++ src/main/java/com/springboot/RestAPI/controller/StudentController.java
@@ -3,11 +3,23 @@ import com.springboot.RestAPI.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     @GetMapping("student")
     public Student getStudent(){
         Student student = new Student(1,"Ravi","Chandola");
         return student;
+    }
+@GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1,"Ram","Srivastav"));
+        students.add(new Student(2,"Girish","Srivastav"));
+        students.add(new Student(3,"Nimish","Srivastav"));
+        students.add(new Student(4,"Dakshita","Srivastav"));
+        return students;
     }
 }
